5ba7d5640fbd02387ab9f7cdf4a05219fa788297
Arkar Daniels <arkardaniels@gmail.com>
2015-07-28 23:07:53 +0800

    Add Beliu.py Setup_0.0.1

---
+++ Beliu.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import socket
+import argparse
+import sys
+
+def Main(interface, port):
+
+	print(str(interface))
+	print(str(port))
+
+
+if __name__ == "__main__":
+	Main('',1060)
